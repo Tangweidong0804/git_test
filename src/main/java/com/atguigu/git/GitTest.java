e7770355f6fe41b7067e7a6f72cb1e60bad58c15
Tangweidong <460990507@qq.com>
2022-03-12 13:32:31 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author Tangweidong
+ * @create 2022-03-12 13:29
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
